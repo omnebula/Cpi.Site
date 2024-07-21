8ea2510a20e37712f1847a9ee4aa56fdcac9afb8
luxabundus <joelluzadas@gmail.com>
2024-07-21 01:42:26 -0400

    (WIP) Initial Roadmap commit

--- root/roadmap.js
+++ root/roadmap.js
@@ -2,8 +2,47 @@
 
 class RoadmapPage extends CpiPage
 {
+    #benchmarkRowTemplate;
+    #coverageRowTemplate;
+
     constructor() {
         super();
+
+        this.#coverageRowTemplate = $("#benchmarkCoverage").detach();
+        this.#benchmarkRowTemplate = $("#benchmarkRow").detach();
+
+        this.sendApiRequest({
+            method: "GET",
+            url: "/@/lesson/coverage?subject=Mathematics&grade=3",
+            success: (data, status, xhr) => {
+                this.#populateResults(data);
+            }
+        });
+    }
+
+    #populateResults(data) {
+        const benchmarkContainer = $("#benchmarkRowContainer");
+        benchmarkContainer.children().remove();
+
+        for (const standardCode in data) {
+            const benchmarkInfo = data[standardCode];
+
+            const benchmarkRow = this.#benchmarkRowTemplate.clone(true);
+            benchmarkRow.find("#standardCode").text(standardCode);
+            benchmarkRow.find("#standardDescription").html(benchmarkInfo.d);
+
+            const coverageContainer = benchmarkRow.find("#benchmarkCoverageContainer");
+            for (const date in benchmarkInfo.c) {
+                const subjects = benchmarkInfo.c[date];
+
+                const coverageRow = this.#coverageRowTemplate.clone(true);
+                coverageRow.find("#coverageDate").text(date);
+                coverageRow.find("#coverageSubject").text(subjects.join(", "));
+                coverageContainer.append(coverageRow);
+            }
+
+            benchmarkContainer.append(benchmarkRow);
+        }
     }
 }
 
