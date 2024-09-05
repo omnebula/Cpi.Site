0c1b1b871bb4c5ef505fc63cd94bf1cf33025409
luxabundus <joelluzadas@gmail.com>
2024-09-05 06:31:22 -0400

    View Account Roadmap

--- root/roadmap.js
+++ root/roadmap.js
@@ -7,6 +7,8 @@ class RoadmapPage extends CpiPage
     #subjectSelector;
     #gradeSelector;
     #scopeSelector;
+    #pageData;
+    #teacherId;
 
     constructor() {
         super();
@@ -21,49 +23,97 @@ class RoadmapPage extends CpiPage
         // Initialize the roadmap table
         this.#roadmapTable = new DataTable($("#roadmapTable"));
 
+
+        const pageDataSetting = localStorage.getItem("roadmapData");
+        if (pageDataSetting) {
+            this.#pageData = JSON.parse(pageDataSetting);
+        }
+        else {
+            this.#pageData = {
+                lastSubject: localStorage.getItem("lastRoadmapSubject") || "",
+                lastGrade: localStorage.getItem("lastRoadmapGrade") || "",
+                lastScope: localStorage.getItem("lastRoadmapScope") || "all"
+            };
+            localStorage.removeItem("lastRoadmapSubject");
+            localStorage.removeItem("lastRoadmapGrade");
+            localStorage.removeItem("lastRoadmapScope");
+
+            this.#savePageData();
+        }
+
+        // Detect administrative access, i.e., from Organization Manager.
+        const searchParams = new URLSearchParams(window.location.search);
+        const referrerUrl = new URL(document.referrer);
+        this.#teacherId = searchParams.get("tid");
+        const teacherName = searchParams.get("tname");
+        if (this.#teacherId && teacherName) {
+            const pageTitleName = $("#pageTitleName");
+            pageTitleName.text(`View ${pageTitleName.text()}:`);
+
+            const pageSubTitle = $("#pageSubTitle");
+            pageSubTitle.text(teacherName);
+            pageSubTitle.css("display", "inline-block");
+
+            $("#myRoadmap").css("display", "inline-block");
+            $(".siteCurrentMenuOption").css("display", "none");
+
+            this.#pageData.lastSubject = "";
+            this.#pageData.lastGrade = "";
+        }
+        else {
+            $("#myRoadmap").css("display", "none");
+        }
+
         /*
         * Initialize selectors
         */
-        const subject = localStorage.getItem("lastRoadmapSubject") || "";
-        const grade = localStorage.getItem("lastRoadmapGrade") || "";
-        const scope = localStorage.getItem("lastRoadmapScope") || "all";
-        
         this.#subjectSelector = $("#subjectSelector");
         this.#gradeSelector = $("#gradeSelector");
         this.#scopeSelector = $("#scopeSelector");
 
         // Subject selector
         this.#subjectSelector.on("change", () => {
-            localStorage.setItem("lastRoadmapSubject", this.#subjectSelector.val());
-            this.#syncSubjectGrades(localStorage.getItem("lastRoadmapGrade"));
+            this.#pageData.lastSubject = this.#subjectSelector.val();
+            this.#syncSubjectGrades(this.#pageData.lastGrade);
             this.#queryRoadmap();
+            this.#savePageData();
         });
 
         // Grade selector
         this.#gradeSelector.on("change", () => {
-            localStorage.setItem("lastRoadmapGrade", this.#gradeSelector.val());
+            this.#pageData.lastGrade = this.#gradeSelector.val();
             this.#queryRoadmap();
+            this.#savePageData();
         });
 
         // Scope selector
-        this.#scopeSelector.val(scope);
+        this.#scopeSelector.val(this.#pageData.lastScope);
         this.#scopeSelector.on("change", () => {
-            localStorage.setItem("lastRoadmapScope", this.#scopeSelector.val());
+            this.#pageData.lastScope = this.#scopeSelector.val();
             this.#queryRoadmap();
+            this.#savePageData();
         })
 
         Cpi.ShowAppFrame();
 
+        //
+        var queryUrl = `/@/lesson/roadmap?wantMeta&subject=${this.#pageData.lastSubject}&grade=${this.#pageData.lastGrade}&scope=${this.#pageData.lastScope}`;
+        if (this.#teacherId) {
+            queryUrl += `&teacherId=${this.#teacherId}`;
+        }
         Cpi.SendApiRequest({
             method: "GET",
-            url: `/@/lesson/roadmap?wantMeta&subject=${subject}&grade=${grade}&scope=${scope}`,
+            url: queryUrl,
             success: (data, status, xhr) => {
-                this.#init(data, subject, grade, scope);
+                this.#init(data,);
             }
         });
     }
 
-    #init(data, currentSubject, currentGrade, currentScope) {
+    #init(data) {
+        var currentSubject = this.#pageData.lastSubject;
+        var currentGrade = this.#pageData.lastGrade;
+
         // Initialize subjects.
         for (const subject of data.meta.subjects) {
             const option = document.createElement("option");
@@ -75,7 +125,6 @@ class RoadmapPage extends CpiPage
         if (!currentSubject || (this.#subjectSelector.val(currentSubject).val() != currentSubject)) {
             currentSubject = this.#subjectSelector.find(":first").val();
         }
-        localStorage.setItem("lastRoadmapSubject", currentSubject);
         this.#subjectSelector.val(currentSubject);
 
         this.#syncSubjectGrades(currentGrade);
@@ -101,19 +150,21 @@ class RoadmapPage extends CpiPage
 
         if (!currentGrade || (this.#gradeSelector.val(currentGrade).val() !== currentGrade)) {
             currentGrade = this.#gradeSelector.find(":first").val();
-            localStorage.setItem("lastRoadmapGrade", currentGrade);
+            this.#pageData.lastGrade = currentGrade;
             this.#gradeSelector.val(currentGrade);
+            this.#savePageData();
         }    
     }
 
     #queryRoadmap() {
-        const subject = this.#subjectSelector.val();
-        const grade = this.#gradeSelector.val();
-        const scope = this.#scopeSelector.val();
+        var queryUrl = `/@/lesson/roadmap?subject=${this.#pageData.lastSubject}&grade=${this.#pageData.lastGrade}&scope=${this.#pageData.lastScope}`;
+        if (this.#teacherId) {
+            queryUrl += `&teacherId=${this.#teacherId}`;
+        }
 
         Cpi.SendApiRequest({
             method: "GET",
-            url: `/@/lesson/roadmap?subject=${subject}&grade=${grade}&scope=${scope}`,
+            url: queryUrl,
             success: (data, status, xhr) => {
                 this.#populateRoadmapTable(data.benchmarks);
             }
@@ -152,6 +203,12 @@ class RoadmapPage extends CpiPage
         }
 
         this.#roadmapTable.stripeRows();
+    }
+
+    #savePageData() {
+        if (!this.#teacherId) {
+            localStorage.setItem("roadmapData", JSON.stringify(this.#pageData));
+        }
     }
 }
 
