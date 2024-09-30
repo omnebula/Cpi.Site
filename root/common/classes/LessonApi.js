8a40597737819b5439bf9cd2352ab653d730229f
luxabundus <joelluzadas@gmail.com>
2024-09-30 06:55:56 -0400

    Auto set print PDF file name

--- root/common/classes/LessonApi.js
+++ root/common/classes/LessonApi.js
@@ -2,14 +2,21 @@
 
 class LessonApi {
     static PrintLesson(params) {
+        const currentTitle = document.title;
+
         function setPrint() {
+            const openPrint = () => {
+                document.title = `${params.name} - ${Cpi.FormatShortDateString(params.date)}`;
+            };
             const closePrint = () => {
                 document.body.removeChild(this);
+                document.title = currentTitle;
             };
 
             this.contentWindow.initPrintout(params);
 
             this.contentWindow.onbeforeunload = closePrint;
+            this.contentWindow.onbeforeprint = openPrint;
             this.contentWindow.onafterprint = closePrint;
             this.contentWindow.print();
         }
